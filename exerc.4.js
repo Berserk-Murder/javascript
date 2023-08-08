67bea631a0ac43cb82c452ecb408294d09ae63f0
Your Name <you@example.com>
2023-08-08 19:58:45 -0300

    Finished

--- exerc.4.js
+++ exerc.4.js
@@ -1,24 +1,16 @@
-function palin(pal){
-    var palindroma = true;
-    pal = pal.toUpperCase();
-  
-    for(var i = 0; i < (pal.length / 2); i++){
-      if(pal.charAt(i) != pal.charAt(pal.length - i - 1)){
-        palindroma = false;
-      }
-    }
-	
-    return palindroma;
-  }
-  
-  var palavra
-  if(palin(palavra)){
-    document.getElementById("palavra").innerHTML = "Esta palavra é palindroma";
-  }
-  else{
-    alert("Esta palavra NÃO é palindroma"); 
-  }
+function verificar() {
+  const palavra = document.getElementById('palavra').value.toLowerCase();
+  const inversa = palavra.split('').reverse().join('');
 
+  document.getElementById('result').innerHTML = inversa
+
+if (palavra == inversa) {
+  document.getElementById("result").innerHTML = "A palavra É um PALÍNDROMO"
+} else {
+  document.getElementById("result").innerHTML = "A palavra NÃO É um PALÍNDROMO"
+
+}
+}
 function limpar() {
-    document.getElementById("result").innerHTML = "idadeDirecao";
+  document.getElementById('result').innerHTML = "";
 }
