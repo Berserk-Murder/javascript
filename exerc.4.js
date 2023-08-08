dfbf664bb3f3d8dcc7b925816612aeee74bad9c7
Your Name <you@example.com>
2023-08-08 20:12:20 -0300

    Finished

--- exerc.4.js
+++ exerc.4.js
@@ -1,15 +1,17 @@
 function verificar() {
   const palavra = document.getElementById('palavra').value.toLowerCase();
+  if (palavra == '') {
+    document.getElementById('result').innerHTML = "Null"
+  }
+  else {
   const inversa = palavra.split('').reverse().join('');
 
-  document.getElementById('result').innerHTML = inversa
-
-if (palavra == inversa) {
-  document.getElementById("result").innerHTML = "A palavra É um PALÍNDROMO"
-} else {
-  document.getElementById("result").innerHTML = "A palavra NÃO É um PALÍNDROMO"
-
-}
+  if (palavra == inversa) {
+    document.getElementById("result").innerHTML = "A palavra É um PALÍNDROMO"
+  } else {
+    document.getElementById("result").innerHTML = "A palavra NÃO É um PALÍNDROMO"
+  }
+  }
 }
 function limpar() {
   document.getElementById('result').innerHTML = "";
